6e49fb24fcbec1687ee39d732e1e6b11572efb5a
Hail Mary <41487770+alejosjen@users.noreply.github.com>
2018-10-17 20:12:58 -0700

    html with image and links to artist work; css with color and font theme; javascript with plan.

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+// Key events to listen for letters typed
+
+//Display "Press any key to get started!"
+    // Event listener for any key to start game
+
+    /* alert("Press any key to get started!"); */
+
+//Record wins, # of times user guessed word correctly
+    //Need Counter
+//Display amount of letters to word
+    //Display letters guessed correctly within word
+
+//Record number of guess remaining
+    //Need Counter
+    //Display guesses
+
+//Game starts new word automatically
+
+//Bonus : Sound when word guessed correctly
+// Organize code as an object, except for the key events to the letter gussed
+        //Save whole game and its properties in an object
+        //Save any funtions as methods, call them underneath object declaration using
+        //  event listeners
+        //Place global variables and functions above object
+        //Order: Global variables, objects, then calls
