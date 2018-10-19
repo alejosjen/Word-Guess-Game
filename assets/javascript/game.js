0f563df9ffdd1730ffb73fe829185c993e3fef45
Hail Mary <41487770+alejosjen@users.noreply.github.com>
2018-10-18 22:08:56 -0700

    the start game has a counter that appears using javascript! it's a keeper

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,65 @@
 // Key events to listen for letters typed
+    
+    /* onkeydown
 
 //Display "Press any key to get started!"
     // Event listener for any key to start game
 
     /* alert("Press any key to get started!"); */
+// Start game
+    function startGame() {
+        document.querySelector(".wins-counter").textContent=winsCounter;
+    }
 
 //Record wins, # of times user guessed word correctly
     //Need Counter
-//Display amount of letters to word
-    //Display letters guessed correctly within word
-
+    winsCounter = 0;
 //Record number of guess remaining
     //Need Counter
+    guessesRemainingCounter = 10;
     //Display guesses
 
+//Create an array of words for game
+    var fairyTaleWords = [
+        "aurora",
+        "briar rose",
+        "maleficent",
+        "prince philip",
+        "flora",
+        "fauna",
+        "merryweather",
+        "king stefan",
+        "spinning wheel",
+        "true loves kiss",
+        "blessings",
+        "fairies",
+        "curse",
+        "beauty",
+        "wit",
+        "grace",
+        "dance",
+        "song",
+        "music",
+        "eyvind earle",
+        "mary costa",
+        "helene stanley"
+    ];
+
+//Display amount of letters to word
+   /*  for (var i=0; i<fairyTaleWords.length; i++) {
+        return fairyTaleWords[0];
+    } */
+
+    
+//Replace each letter with underscore
+    
+
+//Display letters guessed correctly within word
+
+
+
 //Game starts new word automatically
+startGame();
 
 //Bonus : Sound when word guessed correctly
 // Organize code as an object, except for the key events to the letter gussed
